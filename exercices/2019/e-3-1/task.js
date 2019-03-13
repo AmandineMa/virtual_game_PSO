2625043fb14f06da63b345b86e2abc5640fe41fd
ogor <ronan.ogor@artal.fr>
2019-03-13 01:16:25 +0100

    Ajout du niveau 3-1

--- exercices/2019/e-3-1/task.js
+++ exercices/2019/e-3-1/task.js
@@ -2,168 +2,95 @@ function initTask(subTask) {
   var cellSide = 60;
 
   subTask.gridInfos = {
+    backgroundColor: "#d0f4f7",
+    hasGravity: true,
     hideSaveOrLoad: true,
     cellSide: cellSide,
     actionDelay: 200,
     itemTypes: {
       robot: {
-        img: "../images/robot/robot_bleu.png",
+        img: "../images/robot/robot_rose.png",
         side: 80,
         nbStates: 9,
         isObstacle: true,
         offsetX: -14,
         category: "robot",
-        team: 0,
-        zOrder: 3
+        zOrder: 1
       },
       ciel: {
         num: 1,
         img: "../images/fonds/ciel.png",
         side: cellSide,
-        category: "paint",
-        isObstacle: false,
-        hasColor: false,
-        team: 0,
-        zOrder: 0
+        isObstacle: false
       },
 
       caise: {
         num: 2,
         img: "../images/objects/fond_ciel/caise.png",
         side: cellSide,
-        category: "paint",
-        isObstacle: true,
-        hasColor: false,
-        team: 0,
-        zOrder: 0
+        category: "platform",
+        isObstacle: true
       },
-      caisePierre: {
+      caiseCroix: {
         num: 3,
-        img: "../images/objects/fond_ciel/caise_pierrre.png",
+        img: "../images/objects/fond_ciel/caise_croix.png",
         side: cellSide,
-        category: "paint",
-        isObstacle: true,
-        hasColor: false,
-        team: 0,
-        zOrder: 0
+        category: "platform",
+        isObstacle: true
       },
 
       mer: {
         num: 4,
         img: "../images/objects/fond_ciel/mer.png",
         side: cellSide,
-        category: "paint",
-        isObstacle: true,
-        hasColor: false,
-        team: 0,
-        zOrder: 0
+        isObstacle: true
       },
-      terreTop: {
+      terre: {
         num: 5,
         img: "../images/fonds/terre_top.png",
         side: cellSide,
-        category: "paint",
-        isObstacle: true,
-        hasColor: false,
-        team: 0,
-        zOrder: 0
-      },
-      terre: {
-        num: 6,
-        img: "../images/fonds/terre.png",
-        side: cellSide,
-        category: "paint",
-        isObstacle: true,
-        hasColor: false,
-        team: 0,
-        zOrder: 0
+        category: "platform",
+        isObstacle: true
       },
 
-      champi: {
+      cielObstacle: {
         num: 7,
-        img: "../images/objects/fond_ciel/champigon.png",
+        img: "../images/fonds/ciel.png",
         side: cellSide,
-        category: "paint",
-        isObstacle: false,
-        hasColor: false,
-        team: 0,
-        zOrder: 0
+        isObstacle: true
       },
 
       serrure: {
         num: 8,
         img: "../images/objects/fond_ciel/serrure_jaune.png",
         side: cellSide,
-        category: "paint",
-        isObstacle: false,
-        hasColor: false,
-        color: "serrure",
-        team: 0,
-        zOrder: 0
+        category: "serrure",
+        isObstacle: false
       },
       cle: {
         num: 9,
         img: "../images/objects/fond_ciel/cle.png",
         side: cellSide,
-        category: "paint",
+        category: "cle",
         isObstacle: false,
-        hasColor: false,
-        isTransportable: true,
-        color: "cle",
-        team: 0,
-        zOrder: 0
+        isTransportable: true
       },
 
       mouche: {
         num: 10,
         img: "../images/objects/fond_ciel/mouche.gif",
         side: cellSide,
-        category: "paint",
-        isObstacle: false,
-        hasColor: false,
-        team: 0,
-        zOrder: 0
+        isObstacle: false
       },
       n_g: {
         num: 11,
         img: "../images/objects/fond_ciel/nuage_gauche.png",
-        side: cellSide,
-        category: "paint",
-        isObstacle: false,
-        hasColor: false,
-        team: 0,
-        zOrder: 0
+        side: cellSide
       },
       n_d: {
         num: 12,
         img: "../images/objects/fond_ciel/nuage_droit.png",
-        side: cellSide,
-        category: "paint",
-        isObstacle: false,
-        hasColor: false,
-        team: 0,
-        zOrder: 0
-      },
-
-      terrea: {
-        num: 13,
-        img: "../images/objects/fond_ciel/terre_arrondi.png",
-        side: cellSide,
-        category: "paint",
-        isObstacle: true,
-        hasColor: false,
-        team: 0,
-        zOrder: 0
-      },
-      cactus: {
-        num: 14,
-        img: "../images/objects/fond_ciel/cactus.png",
-        side: cellSide,
-        category: "paint",
-        isObstacle: false,
-        hasColor: false,
-        team: 0,
-        zOrder: 0
+        side: cellSide
       }
     },
 
@@ -172,12 +99,11 @@ function initTask(subTask) {
       groupByCategory: false,
       generatedBlocks: {
         robot: [
-          "north",
-          "south",
-          "east",
-          "west",
+          "forward",
+          "turnAround",
+          "jump",
           "pickTransportable",
-          "dropTransportable"
+          "openLocker"
         ]
       },
       standardBlocks: {
@@ -196,24 +122,22 @@ function initTask(subTask) {
       if (lastTurn) {
         var robot = context.getRobotItem(context.curRobot);
 
-        var hasAllKeys = context.nbTransportedItems === 3;
+        var noSerrure =
+          context.getItems(undefined, undefined, { category: "serrure" })
+            .length == 0;
 
-        if (hasAllKeys) {
+        if (noSerrure) {
           context.success = true;
-          throw "Bravo, Goumo a ouvert la serrure !";
+          throw "Bravo, Goumo a ouvert toutes les serrure !";
         } else {
           context.success = false;
-          throw "Goumo n'a pas ouvert la serrure";
+          throw "Goumo n'a pas ouvert toutes les serrure";
         }
       }
     },
     computeGrade: function(context, message) {
-      var rate = 0;
-      if (context.success) {
-        rate = 1;
-      }
       return {
-        successRate: rate,
+        successRate: rcontext.success ? 1 : 0,
         message: message
       };
     }
@@ -223,18 +147,18 @@ function initTask(subTask) {
     easy: [
       {
         tiles: [
-          [1, 11, 12, 1, 10, 1, 1],
-          [1, 1, 1, 1, 1, 11, 12],
-          [11, 12, 1, 1, 9, 1, 1],
-          [1, 1, 1, 3, 3, 1, 1],
-          [1, 1, 1, 1, 9, 1, 1],
-          [1, 1, 1, 3, 3, 1, 1],
-          [1, 1, 1, 1, 9, 1, 7],
-          [1, 1, 13, 5, 5, 5, 5],
-          [14, 1, 1, 6, 6, 6, 6],
-          [5, 5, 5, 6, 6, 6, 6]
+          [9, 1, 1, 1, 1, 1, 8],
+          [2, 2, 2, 2, 2, 2, 2],
+          [9, 1, 1, 1, 1, 1, 8],
+          [2, 2, 2, 2, 2, 2, 2],
+          [9, 1, 1, 1, 1, 1, 8],
+          [2, 2, 2, 2, 2, 2, 2],
+          [9, 1, 1, 1, 1, 1, 8],
+          [2, 2, 2, 2, 2, 2, 2],
+          [1, 1, 1, 1, 1, 1, 1],
+          [5, 5, 5, 5, 5, 5, 5]
         ],
-        initItems: [{ row: 8, col: 1, dir: 1, type: "robot" }]
+        initItems: [{ row: 8, col: 0, dir: 0, type: "robot" }]
       }
     ]
   };
